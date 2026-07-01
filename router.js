0602715346b3d8622451ebf306aeb1da78cc9ac4
kounelis1234-ctrl <kounelis1234@gmail.com>
2026-07-01 17:47:24 +0300

    Update router.js

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const router = new Router({
   "/": "index.html",
   "/projects": "projects.html",
   "/en": "index-en.html",
-  "/projects/en" : "projects-en.html"
+  "/en/projects" : "projects-en.html"
 });
 
 router.start();
